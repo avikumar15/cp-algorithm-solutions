aff72542d4185957cd421fb5813e3fb879fedea5
Avi Kumar <avithewinner1508@gmail.com>
2020-12-27 14:53:04 +0000

    add: alternate solution for CPU Scheduler

--- algorithms/cpp/Medium/Brute_Implementation/CPUScheduler.cpp
+++ algorithms/cpp/Medium/Brute_Implementation/CPUScheduler.cpp
@@ -53,5 +53,20 @@ public:
         
         return ans;
     }
+    
+    // ALTERNATE SOLUTION
+    int leastInterval(vector<char>& tasks, int n) {
+        unordered_map<char,int>mp;
+        int count = 0;
+        for(auto e : tasks)
+        {
+            mp[e]++;
+            count = max(count, mp[e]);
+        }
+        
+        int ans = (count-1)*(n+1);
+        for(auto e : mp) if(e.second == count) ans++;
+        return max((int)tasks.size(), ans);
+    }
 };
 
